73153d4e1e30b3b09982af5945a0c9ed6e4e6411
Kevin Corona <keviinvanhalen@gmail.com>
2022-12-15 01:27:03 -0600

    first commit

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        Home page
+        <v-card class="mt-10">
+            <v-tabs
+                v-model="tab"
+                bg-color="primary"
+            >
+                <v-tab value="one">Item One</v-tab>
+                <v-tab value="two">Item Two</v-tab>
+                <v-tab value="three">Item Three</v-tab>
+            </v-tabs>
+        
+            <v-card-text>
+                <v-window v-model="tab">
+                <v-window-item value="one">
+                    One
+                </v-window-item>
+        
+                <v-window-item value="two">
+                    Two
+                </v-window-item>
+        
+                <v-window-item value="three">
+                    Three
+                </v-window-item>
+                </v-window>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+    import { defineComponent } from "@vue/runtime-core"
+    import { successSweetAlert } from '../helpers/sweetAlertGlobals'
+
+    export default defineComponent({
+        name: 'home',
+        data() {
+            return {
+                tab: null,
+            }
+        },
+        created() {
+            this.probarAlert()
+        },
+        methods: {
+            probarAlert() {
+                successSweetAlert('¡Bienvenido!')
+            }
+        }
+    })
+</script>
---
+++ resources/js/pages/Login.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-6 mt-4">
+
+                <h2>Login</h2>
+                <p class="text-danger" v-if="error">{{ error }}</p>
+
+                <form @submit.prevent="login">
+                    <div class="form-group">
+                        <label for="email">Email Address:</label>
+                        <input type="email" name="email" id="email" class="form-control" v-model="form.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" id="password" class="form-control" v-model="form.password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { reactive, ref } from 'vue'
+    import { useRouter } from 'vue-router'
+    import { useStore } from 'vuex'
+
+    export default {
+        setup() {
+            const router = useRouter()
+            const store = useStore()
+
+            let form = reactive({
+                email: '',
+                password: '',
+            })
+
+            let error = ref('')
+
+            const login = async() => {
+                await axios.post('/api/login', form).then(res => {
+                    if (res.data.success) {
+                        store.dispatch('setToken', res.data.data.token)
+                        router.push({name: 'Dashboard'})
+                    } else {
+                        error.value = res.data.message
+                    }
+                })
+            }
+
+            return {
+                form,
+                login,
+                error,
+            }
+        }
+    }
+</script>
